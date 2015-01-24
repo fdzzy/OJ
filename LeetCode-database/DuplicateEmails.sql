88640a6128f1ea47190fb8ebb20a37b7061a1eda
Joey Zhang <joeyzhang@163.com>
2015-01-24 19:37:03 +0800

    Duplicate Emails

---
+++ LeetCode-database/DuplicateEmails.sql
@@ -0,0 +1,23 @@
+# Duplicate Emails 
+# Write a SQL query to find all duplicate emails in a table named Person.
+# 
+# +----+---------+
+# | Id | Email   |
+# +----+---------+
+# | 1  | a@b.com |
+# | 2  | c@d.com |
+# | 3  | a@b.com |
+# +----+---------+
+# For example, your query should return the following for the above table:
+# 
+# +---------+
+# | Email   |
+# +---------+
+# | a@b.com |
+# +---------+
+
+# Write your MySQL query statement below
+SELECT Email FROM (
+SELECT Email, COUNT(*) AS Count FROM Person GROUP BY Email
+) c 
+WHERE c.Count > 1;
