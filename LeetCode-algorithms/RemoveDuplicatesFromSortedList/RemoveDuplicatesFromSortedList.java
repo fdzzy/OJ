84c9228ba9ff098afd64efec241fa1a3ee151103
Joey Zhang <joeyzhang@163.com>
2015-01-27 13:10:04 +0800

    Remove Duplicates From Sorted List I && II

--- LeetCode-algorithms/RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList.java
+++ LeetCode-algorithms/RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList.java
@@ -20,18 +20,51 @@ public class RemoveDuplicatesFromSortedList {
     	}
     	return head;
     }
+    
+    public ListNode deleteDuplicatesII(ListNode head) {
+    	boolean duplicate = false;
+    	while(true) {
+    		duplicate = false;
+			while (head != null && head.next != null
+					&& head.next.val == head.val) {
+				duplicate = true;
+				head = head.next;
+			}
+			if(duplicate) head = head.next;
+			else break; 
+    	}
+    	if(duplicate) head = head.next;
+    	if(head == null) return null;
+
+    	ListNode node = head;
+    	while(node != null && node.next != null && node.next.next != null) {
+    		if(node.next.val == node.next.next.val) {
+				ListNode iter = node.next.next;
+				while (iter.next != null && iter.next.val == iter.val)
+					iter = iter.next;
+				node.next = iter.next;
+    		} else {
+    			node = node.next;
+    		}
+    	}
+    	
+    	return head;
+    }
 
     public static void main(String[] args) {
 		ListNode node = new ListNode(5);
+		node = ListNode.addNodeAhead(node, 4);
+		node = ListNode.addNodeAhead(node, 4);
+		node = ListNode.addNodeAhead(node, 3);
+		node = ListNode.addNodeAhead(node, 3);
 		node = ListNode.addNodeAhead(node, 2);
-		node = ListNode.addNodeAhead(node, 2);
-		node = ListNode.addNodeAhead(node, 1);
+		//node = ListNode.addNodeAhead(node, 1);
 		node = ListNode.addNodeAhead(node, 1);
 	
 		RemoveDuplicatesFromSortedList r = new RemoveDuplicatesFromSortedList();
-		ListNode head = r.deleteDuplicates(node);
+		//ListNode head = r.deleteDuplicates(node);
+		ListNode head = r.deleteDuplicatesII(node);
 		System.out.println(head);
-		
     }
 
 }
