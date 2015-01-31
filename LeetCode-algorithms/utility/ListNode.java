bb10d658dfa4a535e40a227ed062a58ca5b83fa5
Joey Zhang <joeyzhang@163.com>
2015-01-31 23:03:33 +0800

    add readList

--- LeetCode-algorithms/utility/ListNode.java
+++ LeetCode-algorithms/utility/ListNode.java
@@ -6,4 +6,33 @@ public class ListNode {
 	    val = x;
 	    next = null;
 	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		ListNode node = this;
+		while(node != null) {
+			sb.append(node.val + " ");
+			node = node.next;
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+    public static ListNode addNodeAhead(ListNode node, int val) {
+    	ListNode head = new ListNode(val);
+    	head.next = node;
+    	return head;
+    }
+    
+	public static ListNode readList(int[] nums) {
+		if(nums.length <=0) return null;
+		ListNode head = null;
+		for(int i=nums.length-1; i>=0 ; i--) {
+			ListNode node = new ListNode(nums[i]);
+			node.next = head;
+			head = node;
+		}
+		return head;
+	}
 }
